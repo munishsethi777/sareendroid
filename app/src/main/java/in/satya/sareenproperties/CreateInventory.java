cc6207b048ac5ce417ada5168f5a751ac077c3a5
bgaheer <baljeetgaheer@gmail.com>
2018-04-18 21:04:42 +0530

    changes

--- app/src/main/java/in/satya/sareenproperties/CreateInventory.java
+++ app/src/main/java/in/satya/sareenproperties/CreateInventory.java
@@ -10,6 +10,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -20,10 +22,33 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONObject;
+
 import java.sql.Connection;
 
-public class CreateInventory extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+import in.satya.sareenproperties.Enums.ApprovalType;
+import in.satya.sareenproperties.Enums.DocumentType;
+import in.satya.sareenproperties.Enums.FacingType;
+import in.satya.sareenproperties.Enums.MediumType;
+import in.satya.sareenproperties.Enums.PropertyOfferType;
+import in.satya.sareenproperties.Enums.PropertySideType;
+import in.satya.sareenproperties.Enums.PropertyType;
+import in.satya.sareenproperties.Enums.PropertyUnit;
+import in.satya.sareenproperties.Enums.PurposeType;
+import in.satya.sareenproperties.services.Interface.IServiceHandler;
+
+public class CreateInventory extends AppCompatActivity implements IServiceHandler, OnMapReadyCallback, LocationListener,View.OnClickListener {
     private GoogleMap mMap;
+    private FloatingActionButton fab;
+    private Spinner spinner_property_type;
+    private Spinner spinner_purpose;
+    private Spinner spinner_medium;
+    private Spinner spinner_offer;
+    private Spinner spinner_property_Unit;
+    private Spinner spinner_facing;
+    private Spinner spinner_document_type;
+    private Spinner spinner_approval_type;
+    private Spinner spinner_property_side;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +56,11 @@ public class CreateInventory extends AppCompatActivity implements OnMapReadyCall
         setContentView(R.layout.activity_create_inventory);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-
-
+        buildSpinners();
     }
 
     @Override
@@ -64,11 +79,11 @@ public class CreateInventory extends AppCompatActivity implements OnMapReadyCall
              LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
              Criteria criteria = new Criteria();
              String bestProvider = locationManager.getBestProvider(criteria, true);
-             Location location = locationManager.getLastKnownLocation(bestProvider);
-             if (location != null) {
-                 onLocationChanged(location);
-             }
-             locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
+//             Location location = locationManager.getLastKnownLocation(bestProvider);
+//             if (location != null) {
+//                 onLocationChanged(location);
+//             }
+//             locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
          }catch (SecurityException se){
 
          }
@@ -109,4 +124,84 @@ public class CreateInventory extends AppCompatActivity implements OnMapReadyCall
         }
     }
 
+    private void buildSpinners(){
+        spinner_property_type = (Spinner) findViewById(R.id.property_type);
+        spinner_purpose = (Spinner) findViewById(R.id.purpose);
+        spinner_medium = (Spinner) findViewById(R.id.medium);
+        spinner_offer = (Spinner) findViewById(R.id.propertyoffer);
+        spinner_property_Unit = (Spinner) findViewById(R.id.propertyunit);
+        spinner_facing = (Spinner) findViewById(R.id.facing);
+        spinner_document_type = (Spinner) findViewById(R.id.documentation);
+        spinner_approval_type = (Spinner) findViewById(R.id.approvaltype);
+        spinner_property_side = (Spinner) findViewById(R.id.propertysides);
+
+        spinner_property_type.setAdapter(
+                new ArrayAdapter<PropertyType>(this,
+                        android.R.layout.simple_spinner_item,
+                        PropertyType.values()));
+
+
+        spinner_purpose.setAdapter(
+                new ArrayAdapter<PurposeType>(this,
+                        android.R.layout.simple_spinner_item,
+                        PurposeType.values()));
+
+        spinner_medium.setAdapter(
+                new ArrayAdapter<MediumType>(this,
+                        android.R.layout.simple_spinner_item,
+                        MediumType.values()));
+
+        spinner_offer.setAdapter(
+                new ArrayAdapter<PropertyOfferType>(this,
+                        android.R.layout.simple_spinner_item,
+                        PropertyOfferType.values()));
+
+        spinner_property_Unit.setAdapter(
+                new ArrayAdapter<PropertyUnit>(this,
+                        android.R.layout.simple_spinner_item,
+                        PropertyUnit.values()));
+
+        spinner_facing.setAdapter(
+                new ArrayAdapter<FacingType>(this,
+                        android.R.layout.simple_spinner_item,
+                        FacingType.values()));
+
+        spinner_document_type.setAdapter(
+                new ArrayAdapter<DocumentType>(this,
+                        android.R.layout.simple_spinner_item,
+                        DocumentType.values()));
+
+        spinner_approval_type.setAdapter(
+                new ArrayAdapter<ApprovalType>(this,
+                        android.R.layout.simple_spinner_item,
+                        ApprovalType.values()));
+
+        spinner_property_side.setAdapter(
+                new ArrayAdapter<PropertySideType>(this,
+                        android.R.layout.simple_spinner_item,
+                        PropertySideType.values()));
+    }
+
+
+    private void saveInventory(){
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.fab){
+            saveInventory();
+        }
+    }
+
+    @Override
+    public void processServiceResponse(JSONObject response) {
+
+    }
+
+    @Override
+    public void setCallName(String call) {
+
+    }
 }
--- app/src/main/java/in/satya/sareenproperties/DashboardActivity.java
+++ app/src/main/java/in/satya/sareenproperties/DashboardActivity.java
@@ -1,5 +1,6 @@
 package in.satya.sareenproperties;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -79,11 +80,12 @@ public class DashboardActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            Intent intent = new Intent(this,CreateInventory.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
-
+            Intent intent = new Intent(this,InventoryList.class);
+            startActivity(intent);
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
@@ -93,7 +95,6 @@ public class DashboardActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
